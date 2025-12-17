a0efa5594eebe2c3c4cd1cd43ce9dc8756a4bb2d
Паша Лысаченко <pashalysacenko@MacBook-Air-Pasa.local>
2025-12-17 12:41:48 +0000

    ЛР1: спроба 2

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-layout',
-  standalone: true,          // если этой строки нет — добавь
-  imports: [],
+  standalone: true,
+  imports: [Header, Footer],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
 export class Layout {
-  appTitle = 'Мій вебдодаток';  // тут напиши название своего вебдодатку
+  appTitle = 'Мій вебдодаток';
 }
